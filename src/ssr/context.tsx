8601aa84bdba5c933501bb79719c94372adb6a69
rphansen91 <rphansen91@gmail.com>
2018-12-30 17:35:50 -0800

    test:

--- src/ssr/context.tsx
+++ src/ssr/context.tsx
@@ -1,10 +1,7 @@
-// import { Subtract } from "utility-types";
+import { Subtract } from "utility-types";
 import { Component, createContext, createElement, ComponentType, SFC } from "react";
 import { DataClient } from "../utils/Interfaces";
 import createDataClient from "./client";
-
-type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
-type Subtract<T, K> = Omit<T, keyof K>;
 
 const React = { createElement }; 
 const initialClient: DataClient = createDataClient();
@@ -15,9 +12,9 @@ interface InjectedSSRDataClientProps {
 }
 
 export const withSSRDataClient = <P extends InjectedSSRDataClientProps>(
-    Cmp: ComponentType<P & InjectedSSRDataClientProps>
+    Cmp: ComponentType<P>
 ) =>
-    class WithSSRDataClient extends Component<Exclude<keyof P, keyof InjectedSSRDataClientProps>> {
+    class WithSSRDataClient extends Component<Subtract<P, InjectedSSRDataClientProps>> {
         render () {
             const props = this.props as unknown as P;
             return (
